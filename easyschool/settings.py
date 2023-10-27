9f62bdcbb446d202b5e03363fe6c815e90ecdbb4
SHAMANTH M H <67217224+shamanthmh@users.noreply.github.com>
2023-10-27 20:48:04 +0530

    Update settings.py

--- easyschool/settings.py
+++ easyschool/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'dpe26%-k#-q^gajj1g8kzurcq%)1_at&u6ne5$*n#zo%#50lcs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '15.207.21.123']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '13.232.103.225']
 
 # Application definition
 
